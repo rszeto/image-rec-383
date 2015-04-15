62431196cc90f58b51cb71ba8a37c6ca0feb00a5
Ryan Szeto <rszeto@umass.edu>
2015-04-14 20:19:16 -0400

    Added more model initialization examples

--- imrecExample.py
+++ imrecExample.py
@@ -1,13 +1,14 @@
 # Useful general Python modules
 from __future__ import division
 import math
-import os
+from os.path import isfile
 
 # Useful modules for working with matrices and ML models
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.naive_bayes import GaussianNB
 from sklearn.datasets import fetch_mldata
+from sklearn.tree import DecisionTreeClassifier
 
 # Module for building the MNIST data sets
 import datasetBuilder
@@ -15,7 +16,7 @@ import datasetBuilder
 ############# Part of the script that does stuff #############
 
 # Create loadable training and test set files if they do not exist
-if not os.path.isfile('train.npy') or not os.path.isfile('test.npy'):
+if not isfile('train.npy') or not isfile('test.npy'):
     print 'Did not find data sets. Creating them now.'
     datasetBuilder.createDatasets()
     print 'Finished creating data sets. Now analyzing data.'
@@ -25,12 +26,15 @@ else:
 # Load training and test data
 train = np.load('train.npy')
 test = np.load('test.npy')
+
+'''
 # Get an image and reshape it from 784x1 vector to 28x28 matrix
 image = train[0, 1:].reshape((28, 28))
 # Show the image
 plt.gray()
 plt.imshow(image)
 plt.show()
+'''
 
 # Get indexes for dividing training data into model training
 # and validation sets
@@ -50,5 +54,18 @@ classifier = GaussianNB()
 # Fit the model to the training data
 classifier.fit(trainData, trainLabels)
 # Evaluate the accuracy of the learned model on the validation set
+score = classifier.score(validationData, validationLabels)
+print score
+
+# Example of initializing model with hyperparameters
+classifier = DecisionTreeClassifier(max_features='sqrt')
+classifier.fit(trainData, trainLabels)
+score = classifier.score(validationData, validationLabels)
+print score
+
+# Example of initializing model with hyperparameters using a dictionary
+dict = {'max_features': 'sqrt'}
+classifier = DecisionTreeClassifier(**dict)
+classifier.fit(trainData, trainLabels)
 score = classifier.score(validationData, validationLabels)
 print score
